55e237e6c05597cf4999d5c483789db8e5ba08e1
amkumar18 <amkumar.sah@gmail.com>
2016-03-23 16:38:12 +0530

    Added BootLogger for Logging using Spring AOP

---
+++ src/main/java/com/celcom/utils/BootLogger.java
@@ -0,0 +1,32 @@
+package com.celcom.utils;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class BootLogger {
+	
+	private final Log log = LogFactory.getLog(getClass());
+	
+	public BootLogger(){
+		
+	}
+	
+    @AfterReturning("execution(* com.celcom..*.*(..))")
+    public void logMethodAccessAfter(JoinPoint joinPoint) {
+        log.info("***** Completed: " + joinPoint.getSignature().getName() + " *****");
+    }
+    
+    @Before("execution(* com.celcom..*.*(..))")
+    public void logMethodAccessBefore(JoinPoint joinPoint) {
+        log.info("***** Starting: " + joinPoint.getSignature().getName() + " *****");
+    }
+
+
+}
